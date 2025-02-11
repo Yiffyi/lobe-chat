df346fdfa032a4674e0bad86cbe91b1c9e9f380a
Yiffyi Jia <git@yif.fyi>
2025-02-10 22:31:19 -0800

    add: allow rebranding via env

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -4,10 +4,10 @@
 
 export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = process.env.BRANDING_NAME || 'LobeChat';
+export const BRANDING_LOGO_URL = process.env.BRANDING_LOGO_URL || '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = process.env.ORG_NAME || 'LobeHub';
 
 export const BRANDING_URL = {
   help: undefined,
